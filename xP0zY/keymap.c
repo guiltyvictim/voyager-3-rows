ef4779e8366dd770b74bf65d526b0a3be4aab4f7
bchfundraising <guiltyvictim@gmail.com>
2024-12-09 14:16:02 +0000

    Adding Magic Keycode QK_MAGIC_TOGGLE_CTL_GUI on Layer 2

--- xP0zY/keymap.c
+++ xP0zY/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [2] = LAYOUT_voyager(
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
-    KC_NO,          KC_EXLM,        KC_AT,          KC_HASH,        KC_DLR,         KC_PERC,                                        KC_MINUS,       KC_7,           KC_8,           KC_9,           KC_NO,          KC_NO,          
+    QK_MAGIC_TOGGLE_CTL_GUI,          KC_EXLM,        KC_AT,          KC_HASH,        KC_DLR,         KC_PERC,                                        KC_MINUS,       KC_7,           KC_8,           KC_9,           KC_NO,          KC_NO,          
     TO(6),          KC_CIRC,        KC_AMPR,        KC_ASTR,        KC_KP_DOT,      KC_EQUAL,                                       KC_0,           KC_4,           KC_5,           KC_6,           KC_ASTR,        KC_NO,          
     TO(1),          KC_NO,          KC_NO,          KC_NO,          KC_GRAVE,       KC_NO,                                          KC_PLUS,        KC_1,           KC_2,           KC_3,           KC_TRANSPARENT, KC_NO,          
                                                     KC_TRANSPARENT, KC_TRANSPARENT,                                 LALT(LGUI(LCTL(LSFT(KC_SPACE)))),LGUI(KC_SPACE)
